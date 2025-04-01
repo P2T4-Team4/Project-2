8f8956ce120539def583273c9db0e08f321bc948
DoyleMartin <Doylecmartin@gmail.com>
2025-03-31 21:19:43 -0400

    trial
    Co-authored-by: Ryan Gayle <ryang2386@users.noreply.github.com>
    Co-authored-by: cdgrosso <cdgrosso@users.noreply.github.com>

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,44 +3,21 @@ dotenv.config();
 
 import { Sequelize } from 'sequelize';
 
-let sequelize;
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+      process.env.DB_NAME || '',
+      process.env.DB_USER || '',
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'postgres',
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
 
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME || '',
-    process.env.DB_USER || '',
-    process.env.DB_PW || '',
-    {
-      host: 'localhost',
-      dialect: 'postgres',
-    },
-  );
-}
-
-
-
-
-
-
-// First starter code
-
-// const sequelize = process.env.DB_URL
-//   ? new Sequelize(process.env.DB_URL)
-//   : new Sequelize(
-//       process.env.DB_NAME || '',
-//       process.env.DB_USER || '',
-//       process.env.DB_PASSWORD,
-//       {
-//         host: 'localhost',
-//         dialect: 'postgres',
-//         dialectOptions: {
-//           decimalNumbers: true,
-//         },
-//       }
-//     );
-
-// export default sequelize;
+export default sequelize;
 
 
