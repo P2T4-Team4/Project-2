464b8eb33eb2b403b37eacfc87bb331728bd0ffb
DoyleMartin <Doylecmartin@gmail.com>
2025-03-31 20:50:43 -0400

    commit try

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -20,21 +20,4 @@ const sequelize = process.env.DB_URL
 
 export default sequelize;
 
-// Render recomends using the following code snippet
 
-
-// let sequelize;
-
-// if (process.env.DB_URL) {
-//   sequelize = new Sequelize(process.env.DB_URL);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PW,
-//     {
-//       host: 'localhost',
-//       dialect: 'postgres',
-//     },
-//   );
-// }
